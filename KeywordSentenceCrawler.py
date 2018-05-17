1b01f47bbe68148d2a05bb1f7a3fbdbec3494eaa
NB29979 <ctwc0162@mail4.doshisha.ac.jp>
2018-05-17 19:22:56 +0900

    アクセスしたサイトのテキストをファイル出力できるように

--- KeywordSentenceCrawler.py
+++ KeywordSentenceCrawler.py
@@ -1,9 +1,7 @@
 # coding: utf-8
-import requests
 from bs4 import BeautifulSoup
 import re
-import chardet
-from selenium.webdriver.chrome.options import Options
+import codecs
 from selenium import webdriver
 
 
@@ -14,16 +12,17 @@ def access_url(_url, _depth):
     driver.get(_url)
     visited_sites.append(_url)
 
+    # webページのテキストの抽出とファイル出力
     result = driver.find_element_by_tag_name('div').text
-    with open('web_page_texts', 'a') as output:
+    with codecs.open('web_page_texts', 'a', 'cp932', 'ignore') as output:
         output.writelines(result)
 
+    # リンクを探して踏む
     soup = BeautifulSoup(driver.page_source, 'html.parser')
     links = [link.get('href') for link in soup.find_all('a', href=re.compile('html$'))]
     print(links)
     print('by: '+_url)
 
-    # todo: テキストの抽出,保存
     # todo: 見つかった<link>だけのstackを用意
     # todo: stack.pop()してvisited_urlsに存在しなければ.top()のurlをaccessed_urlsに追加，
     # access_url(_url, _depth+1)
@@ -35,8 +34,6 @@ driver = webdriver.Chrome()
 root_links = open('root_links', 'r')
 url_stack = list(set(root_links.readlines()))
 visited_sites = []
-
-access_url('http://www.doshisha.ac.jp/information/campus/access/shinmachi.html',1)
 
 for i in range(len(url_stack)):
     url = url_stack[-1]
